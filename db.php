6e896b156d780670e3d9fb85930598ffbaaea962
Киселёв Феликс Федорович <kff@main.gto.by>
2023-06-07 13:42:47 +0300

    first commit

---
+++ db.php
@@ -0,0 +1,44 @@
+<?php
+	function connect() {
+
+		$link = mysqli_connect("localhost", "root", "root");
+
+		if ($link == false){
+			print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+		}
+		else {
+			$link->query("SET NAMES utf8");
+		}
+		return $link;
+	}
+	
+	$upd_script = "
+		if (this.textContent != this.abbr) {
+			alert(this.id + '\\n' + this.textContent + '\\n' + this.abbr);
+			alert('=!!=');
+			alert('=2=');
+		}
+	";
+
+	function list_query($result, $cols, $a_ops = []) {
+		$options = ['border'=>1, 'cellpadding'=>35, 'cellspacing'=>25];				
+		
+		
+		foreach ($a_ops as $key => $value) {$options[$key] = $value;};				
+		echo '<table ';
+		foreach ($options as $key => $value) {echo $key . '=' . $value;};				
+		echo '><tr><th>' . implode('</th><th>', $cols) . '</th></tr>';
+		foreach($result as $row) {
+			echo "<tr>";
+			foreach($cols as $col)				
+				//echo sprintf('<td id="%s" abbr="%s" contenteditable onblur="alert(this.id + \'\\n\' + this.textContent + \'\\n\' + this.abbr)">%s</td>', $row["Номер"], $row[$col], $row[$col]);
+				echo sprintf('<td id="%s" abbr="%s" contenteditable onblur="%s"> %s </td>', $row["Номер"], $row[$col], $GLOBALS['upd_script'], $row[$col]);
+				//echo sprintf('<td id="%s" abbr="%s" contenteditable onblur="alert(\'==\')"> %s </td>', $row["Номер"], $row[$col], $row[$col]);
+			echo "</tr>";
+		}
+	}
+	// var s; for (var key in document.activeElement) { s += key + \'\\n\'; };  document.write(s+\'\\n\'); 
+	// for (var key in document.activeElement) { if (document.activeElement[key] !== undefined) document.write(key + \' - (\' + document.activeElement[key]) + document.writeln(\'\n)    ---    \'); alert(document.activeElement.text())};
+	// alert(document.activeElement.textContent)
+	// alert(this.id + \'\\n\' + this.textContent)
+?>
